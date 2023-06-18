3815629ee3b7e59640858452a43b6f55d96ac4ab
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-18 18:02:48 +0300

    pluralize assignment - Done

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// Good Luck 💪🏾
+
+
+
+
+
+function pluralize(words) {
+    var wordCount = {};
+    var pluralWords = [];
+  
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+  
+      if (wordCount[word]) {
+        var pluralForm = word + 's';
+        if (!wordCount[pluralForm]) {
+          var singularIndex = pluralWords.indexOf(word);
+          pluralWords[singularIndex] = pluralForm; 
+          wordCount[pluralForm] = true;
+        }
+      } else {
+        wordCount[word] = true;
+        pluralWords.push(word); 
+      }
+    }
+  
+    return pluralWords;
+  }
+
+  var words = ['Cup', 'plate', 'bowl', 'Cup', 'spoon', 'spoon'];
+var pluralWords = pluralize(words);
+
+console.log(pluralWords);
